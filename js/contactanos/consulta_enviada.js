a99085f3d0ddaba24359b4809439358e851af618
Lucas Ulcen <lu.ulcen@duocuc.cl>
2021-05-20 17:24:47 -0400

    Se añadio completamente la validacion y la ventana emergente de contactanos

--- js/contactanos/consulta_enviada.js
+++ js/contactanos/consulta_enviada.js
@@ -1,4 +1,16 @@
 function messageConsultaEnviada() {
 
-    window.alert("Consulta enviada satisfactoriamente!!! ")
+    var nombre = $("#id_txtNombre").val();
+    var correo = $("#id_txtCorreo").val();
+    var escriba = $("#id_txtEscriba").val();
+
+    var larNombre = nombre.length;
+    var larCorreo = correo.length;
+    var larEscriba = escriba.length;
+
+    if ( (larNombre >= 5) && (larCorreo >= 5) && (larEscriba >= 30)
+    ) {
+        window.alert("Consulta enviada satisfactoriamente!!! ")
+    }
+ 
 }
